c7e020a6a980146e6f4ebdbba585a379f029eb85
fredrik80 <59136674+fredrik80@users.noreply.github.com>
2024-07-23 12:27:03 +0200

    Create 13.py

---
+++ 13.py
@@ -0,0 +1,25 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        translation = {
+            "I" : 1,
+            "V" : 5,
+            "X" : 10,
+            "L" : 50,
+            "C" : 100,
+            "D" : 500,
+            "M" : 1000,
+        }
+
+        number = 0
+
+        number = 0
+
+        for i in range(len(s)-1):
+            if translation[s[i]] < translation[s[i+1]]:
+                number -= translation[s[i]]
+            else:
+                number += translation[s[i]]
+
+        return number+translation[s[-1]]
+        
+        
