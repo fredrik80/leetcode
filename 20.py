f6e0ed11aa342bbc96bddb507f31705c52716a09
fredrik80 <59136674+fredrik80@users.noreply.github.com>
2024-07-23 12:35:33 +0200

    Create 20.py

---
+++ 20.py
@@ -0,0 +1,19 @@
+# 20. Valid Parentheses
+# Easy
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        bracket_map = {")" : "(", "]" : "[", "}" : "{"}
+
+        for char in s:
+            if char in bracket_map:
+                top = stack.pop() if stack else "#"
+
+                if bracket_map[char] != top:
+                    return False
+            
+            else:
+                stack.append(char)
+
+        return not stack
